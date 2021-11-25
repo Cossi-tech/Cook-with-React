13bc2969843b1e3e379470d2d98cdd42c6bc2ab0
Cossi-tech <84704890+Cossi-tech@users.noreply.github.com>
2021-11-25 23:14:41 +0100

    Functional component setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Counter from './Counter.js';
+import CounterHooks from './CounterHooks';
 
 function App() {
   return (
+    <> 
+    Function component
+    <CounterHooks initialCount={0} />
+    Class component
     <Counter initialCount={0}/>
+    </>
   )
 } 
 
