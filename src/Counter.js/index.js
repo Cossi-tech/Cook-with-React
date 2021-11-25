f062bec5be4d387ab9c1d4045297aa44123c53f6
Cossi-tech <84704890+Cossi-tech@users.noreply.github.com>
2021-11-25 15:58:52 +0100

    pratice Class component with state

--- src/Counter.js/index.js
+++ src/Counter.js/index.js
@@ -4,25 +4,28 @@ import {
 
 export default class Counter extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
 
         this.state = {
             count: props.initialCount
-            }
+        }
     }
-   
+
     render() {
 
         return (
             <div>
-                <button onClick = {()=> this.changeCount(-1)}> - </button>
+                <button onClick={() => this.changeCount(-1)}> - </button>
                 <span> {this.state.count} </span>
-                <button onClick = {()=> this.changeCount(+1)}> + </button>
+                <button onClick={() => this.changeCount(+1)}> + </button>
             </div>
-            )
+        )
     }
-changeCount(amount){
-    this.setState({count:this.state.count + amount})
+    changeCount(amount) {
+        this.setState(prevState => {
+            return { count: prevState.count + amount }
+    })
+    
 }
 }
