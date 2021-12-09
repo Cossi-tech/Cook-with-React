c8baeca2bd170d6b4234ddb72e6b0a7478ff0aa6
Cossi-tech <84704890+Cossi-tech@users.noreply.github.com>
2021-12-09 10:37:58 +0100

    Add of a recipe list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React,{useState} from 'react';
-import './App.css';
-import Counter from './Counter.js';
-import CounterHooks from './CounterHooks';
-
-export const ThemeContext = React.createContext()
+import RecipeList from "./RecipeList/RecipeList";
 
 function App() {
-
-  const [theme,SetTheme]= useState('red')
   return (
-    <ThemeContext.Provider value={{backgroundColor : theme}}> 
-    Function component
-    <CounterHooks initialCount={0} />
-    Class component
-    <Counter initialCount={0}/>
-
-    <button onClick={() => SetTheme(prevTheme => {
-      return prevTheme ==='red' ? 'blue' : 'red'
-    })}> Toggle Theme </button>
-    
-    </ThemeContext.Provider>
+    <RecipeList  recipes ={sampleRecipes} />
   )
 } 
+const sampleRecipes =[
+  {
+    id : 1,
+    name : 'Plain chicken',
+    servings : 3,
+    cookTime : '1.45',
+    instruction : "1. Put salt on chicken \n2. Put chicken in oven\n 3. Eat chicken",
+    ingredients:[
+      {
+        id : 1,
+        name : 'chicken',
+        amount: '2 Ponds'
+      },
+      {
+        id : 2,
+        name : 'Salt',
+        amount: '1 Tbs'
+      }
+    ]
+  },
+  {
+    id : 2,
+    name : 'Plain Pork',
+    servings : 5,
+    cookTime : '0.45',
+    instruction : "1. Put paprika on Pork \n2. Put pork in oven\n 3. Eat pork",
+    ingredients:[
+      {
+        id : 1,
+        name : 'pork',
+        amount: '3 Ponds'
+      },
+      {
+        id : 2,
+        name : 'Paprika',
+        amount: '2 Tbs'
+      }
+    ]
+  }
+]
 
 export default App;
