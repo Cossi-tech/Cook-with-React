e522f2b5e80ea5f077545aa36e4a5e00471e7d0c
Cossi-tech <84704890+Cossi-tech@users.noreply.github.com>
2021-12-16 23:38:41 +0100

    Add of useContext Hook

--- src/component/App.js
+++ src/component/App.js
@@ -1,74 +1,81 @@
 import RecipeList from "../component/RecipeList/RecipeList";
 import { useState } from "react";
 import '../css/app.css'
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
+import React from "react"; 
+
+export const RecipeContext = React.createContext()
+
 
 function App() {
-  const [recipes, setRecipes]= useState(sampleRecipes)
+  const [recipes, setRecipes] = useState(sampleRecipes)
 
-  function  handleRecipeAdd(){
-  
+  const recipeContextValue = {
+    handleRecipeAdd,
+    handleRecipeDelete
+  }
+  function handleRecipeAdd() {
+
     const newRecipe = {
-      id : uuidv4(),
+      id: uuidv4(),
       name: 'New',
       servings: 1,
       cookTime: '1:00',
       instruction: 'Instr.',
-      ingredients:[
-        { id: uuidv4(), name:'Name', amount:'1 Tbs' }
+      ingredients: [
+        { id: uuidv4(), name: 'Name', amount: '1 Tbs' }
       ]
     }
     setRecipes([...recipes, newRecipe])
-} 
-function handleRecipeDelete(id){
-  setRecipes(recipes.filter(recipe => recipe.id!==id))
-}
+  }
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter(recipe => recipe.id !== id))
+  }
   return (
-    <RecipeList  
-    recipes = {recipes}
-    handleRecipeAdd = {handleRecipeAdd}
-    handleRecipeDelete ={handleRecipeDelete}
-    
-    />
+    <RecipeContext.Provider value={recipeContextValue}>
+
+      <RecipeList recipes={recipes}/>
+    </RecipeContext.Provider>
+
   )
-  
+
 
 }
-const sampleRecipes =[
+const sampleRecipes = [
   {
-    id : 1,
-    name : 'Plain chicken',
-    servings : 3,
-    cookTime : '1.45',
-    instruction : "1. Put salt on chicken \n2. Put chicken in oven\n3. Eat chicken",
-    ingredients:[
+    id: 1,
+    name: 'Plain chicken',
+    servings: 3,
+    cookTime: '1.45',
+    instruction: "1. Put salt on chicken \n2. Put chicken in oven\n3. Eat chicken",
+    ingredients: [
       {
-        id : 1,
-        name : 'Chicken',
+        id: 1,
+        name: 'Chicken',
         amount: '2 Ponds'
       },
       {
-        id : 2,
-        name : 'Salt',
+        id: 2,
+        name: 'Salt',
         amount: '1 Tbs'
       }
     ]
   },
   {
-    id : 2,
-    name : 'Plain Pork',
-    servings : 5,
-    cookTime : '0.45',
-    instruction : "1. Put paprika on Pork \n2. Put pork in oven\n3. Eat pork",
-    ingredients:[
+    id: 2,
+    name: 'Plain Pork',
+    servings: 5,
+    cookTime: '0.45',
+    instruction: "1. Put paprika on Pork \n2. Put pork in oven\n3. Eat pork",
+    ingredients: [
       {
-        id : 1,
-        name : 'Pork',
+        id: 1,
+        name: 'Pork',
         amount: '3 Ponds'
       },
       {
-        id : 2,
-        name : 'Paprika',
+        id: 2,
+        name: 'Paprika',
         amount: '2 Tbs'
       }
     ]
